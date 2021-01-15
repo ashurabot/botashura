4d19de07b74bd4e8c84ba0057df679a56febc9f0
ashurabot <77414400+ashurabot@users.noreply.github.com>
2021-01-15 16:33:53 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,97 @@
+const help = (prefix) => {
+	return `
+
+TRADUZIDO BY ICARO
+DUVIDAS?:wa.me//5527992752045
+
+
+┏━━❉ *𝙎𝙊𝘽𝙍𝙀 𝙊 𝘽𝙊𝙏* ❉━━┓
+┣⊱ *${prefix}owner*  *DONO*
+┣⊱ *${prefix}donate* *DOAR OFF
+┣⊱ *${prefix}info* *INFORMAÇÕES*
+┣⊱ *${prefix}botstat* *STATUS DO BOT*
+┣⊱ *${prefix}bug* *RELATAR UM BUG*
+┣━━❀ *𝘾𝙍𝙀𝘼𝙏𝙀* ❀━━
+┣⊱ *${prefix}ocr*
+┣⊱ *${prefix}sticker*
+┣⊱ *${prefix}tts*
+┣⊱ *${prefix}toimg*
+┣⊱ *${prefix}phlogo*
+┣⊱ *${prefix}quotemaker*
+┣⊱ *${prefix}ninjalogo*
+┣⊱ *${prefix}glitch*
+┣⊱ *${prefix}text3d*
+┣━━❀━━━━❀━━━━❀━━━━━
+┣━━❀ *𝘿𝙄𝙑𝙀𝙍𝙎𝘼𝙊* ❀━━━━
+┣⊱ *${prefix}meme*
+┣⊱ *${prefix}memeindo*
+┣⊱ *${prefix}hilih*
+┣⊱ *${prefix}fitnah*
+┣⊱ *${prefix}truth*
+┣⊱ *${prefix}dare*
+┣⊱ *${prefix}bucin*
+┣⊱ *${prefix}persengay*
+┣━━❀ *𝘼𝙉𝙄𝙈𝙀* ❀━━
+┣⊱ *${prefix}loli*
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}anime*
+┣⊱ *${prefix}randomanime*
+┣⊱ *${prefix}randomhentai*
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}nsfwblowjob*
+┣⊱ *${prefix}nsfwneko*
+┣⊱ *${prefix}nsfwtrap*
+┣━━❀ *𝙈𝙀𝘿𝙄𝘼* ❀━━
+┣⊱ *${prefix}tiktokstalk*
+┣⊱ *${prefix}ytmp4*
+┣━━❀ *𝙊𝙐𝙏𝙍𝙊𝙎* ❀━━
+┣⊱ *${prefix}ping*
+┣⊱ *${prefix}shorturl*
+┣⊱ *${prefix}fototiktok*
+┣⊱ *${prefix}map*
+┣⊱ *${prefix}kbbi*
+┣⊱ *${prefix}artinama*
+┣⊱ *${prefix}infogempa*
+┣⊱ *${prefix}quotes*
+┣⊱ *${prefix}cerpen*
+┣⊱ *${prefix}lirik*
+┣⊱ *${prefix}pokemon*
+┣⊱ *${prefix}chord*
+┣⊱ *${prefix}blocklist*
+┣⊱ *${prefix}hidetag*
+┣⊱ *${prefix}wolflogo*
+┣⊱ *${prefix}lionlogo*
+┣⊱ *${prefix}ssweb*
+┣⊱ *${prefix}anjing*
+┣⊱ *${prefix}indohot*
+┣━━❀ *𝙂𝙍𝙐𝙋𝙊* ❀━━
+┣⊱ *${prefix}linkgc*
+┣⊱ *${prefix}tagall*
+┣⊱ *${prefix}add* [+62xx]
+┣⊱ *${prefix}group* [buka/tutup]
+┣⊱ *${prefix}demote* [tag]
+┣⊱ *${prefix}promote* [tag]
+┣⊱ *${prefix}kick*
+┣⊱ *${prefix}listadmin*
+┣⊱ *${prefix}simih* [1/0]
+┣⊱ *${prefix}nsfw* [1/0]
+┣⊱ *${prefix}welcome* [1/0]
+┣━━❀ *Owner bot* ❀━
+┣⊱ *${prefix}clearall*
+┣⊱ *${prefix}block*
+┣⊱ *${prefix}unblock*
+┣⊱ *${prefix}bc*
+┣⊱ *${prefix}leave*
+┣⊱ *${prefix}setpp*
+┣⊱ *${prefix}setprefix*
+┣━━━━━━━━━━━━━━
+┃*𝘼𝙎𝙃𝙐𝙍𝘼 𝙊 𝙂𝙊𝙎𝙏𝙊𝙎𝙊 𝘿𝘼 𝙏𝙃𝘼𝙈𝙄𝙍𝙀𝙎*
+┗━━━━━━━━━━━━━━
+
+`
+}
+
+exports.help = help
+
+
+
